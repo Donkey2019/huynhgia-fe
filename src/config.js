8d4efbbc986821058cd6a773d36739f01dc05137
hqtin <hqtin@tma.com.vn>
2025-09-12 22:57:26 +0700

    Update be URL

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Cấu hình API cho xuất vật nuôi
 export default {
-  EXPORT_API_URL: 'https://huynhgia-be.onrender.com/api',
+  EXPORT_API_URL: 'https://huynhgia-be-production.up.railway.app/api',
 }; 
